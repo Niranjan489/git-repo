c70c0019c76d7c64c65299dd5b090dcf6ff84059
Niranjan489 <84126143+Niranjan489@users.noreply.github.com>
2022-06-20 11:16:17 +0530

    Moving code to github

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+header('Content-disposition: attachment; filename=image.jpg');
+header('Content-type: image/jpeg');
+readfile('/server/path/to/image.jpg');
+
+function save_image($inPath,$outPath)
+{ //Download images from remote server
+    $in=    fopen($inPath, "rb");
+    $out=   fopen($outPath, "wb");
+    while ($chunk = fread($in,8192))
+    {
+        fwrite($out, $chunk, 8192);
+    }
+    fclose($in);
+    fclose($out);
+}
+
+save_image('http://hks-intlgroup.com/images/logo.png','image.jpg');
